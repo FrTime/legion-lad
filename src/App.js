4b94d8814aae65bc3e68e323617b4ee9170b6903
Tim Mace <timothy.mace@outlook.com>
2019-10-01 12:54:24 -0400

    moar tests

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 function App() {
 
   console.log("process.env:", process.env)
+  console.log(process.env.REACT_APP_TEST_ENV)
+  console.log(process.env.OTHER_TEST_ENV)
   return (
     <div className="App">
       <header className="App-header">
