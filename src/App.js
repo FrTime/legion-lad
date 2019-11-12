fc5917d3d1f6b3a9f0338a067c8031c989dd4729
Tim Mace <timothy.mace@outlook.com>
2019-11-12 14:03:18 -0500

    Took out console logs

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import './App.css';
 
 function App() {
 
-  console.log("process.env:", process.env)
-  console.log(process.env.REACT_APP_TEST_ENV)
-  console.log(process.env.OTHER_TEST_ENV)
   return (
     <div className="App">
       <header className="App-header">
